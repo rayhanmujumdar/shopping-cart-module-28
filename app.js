45d14bdf9135d56cbba6878d0aba4def38828c3b
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-14 21:37:54 +0600

    case price added

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-document.getElementById("value-update").addEventListener("click",function(){
-    // console.log("hello rayhan")
+function updateCaseNumber(isIncreasing){
+    // debugger;
     const inputValue = document.getElementById("input-value");
-    const updateValue = parseInt(inputValue.value) + 1;
-    inputValue.value = updateValue;
-    console.log(inputValue.value)
+    let caseNumber = inputValue.value
+    if(isIncreasing == true){
+        caseNumber = parseInt(inputValue.value) + 1;
+    }
+    else if(caseNumber > 0){
+        caseNumber = parseInt(caseNumber) - 1
+    }
+    inputValue.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById("case-total");
+    caseTotal.innerHTML = caseNumber * 59;
+    console.log(caseNumber);
+    console.log(caseTotal.innerHTML)
+}
+document.getElementById("value-update").addEventListener("click",function(){
+    updateCaseNumber(true);
+})
+document.getElementById("case-minus").addEventListener("click",function(){
+    updateCaseNumber(false);
 })
