9421aea314a4e7eaf956f4bb97d5f9183af15e20
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-02-14 14:58:00 +0600

    new added

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('I am in a separate file')
+document.getElementById("value-update").addEventListener("click",function(){
+    // console.log("hello rayhan")
+    const inputValue = document.getElementById("input-value");
+    const updateValue = parseInt(inputValue.value) + 1;
+    inputValue.value = updateValue;
+    console.log(inputValue.value)
+})
